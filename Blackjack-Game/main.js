817ddc96243bd8b3319962a88ea158e3789b8c96
Ivan Gyimah <ivangyimah@ivans-mbp.mynet>
2022-10-27 18:39:26 +0100

    interim update

--- Blackjack-Game/main.js
+++ Blackjack-Game/main.js
@@ -3,12 +3,14 @@
 // Create two variables firstCard and secondCard
 //Set value to random number between 2-11
 
-let firstCard = 10;
+//VARIABLES SECTION
+//----------------------
+
+let firstCard = 100;
 let secondCard = 11;
-
-
-// Create a variable sum, and set it to the sum of the two cards
-
+let userHasBlackJack = false
+let userIsAlive = true
+let message = null
 let sum = firstCard + secondCard;
 
 console.log(sum)
@@ -16,14 +18,18 @@ console.log(sum)
 // Logic of the game
 
 if (sum < 21) {
-    console.log("Draw a new card? 😊")
+    message=("Draw a new card? 😊")
     }
     
     else if (sum === 21) {
-      console.log("You've got Blackjack! 🥳")
+      message=("You've got Blackjack! 🥳")
+      userHasBlackJack = true
     }
 
     else if (sum > 21) {
-        console.log("You've lost! You're out the game. 😢")
+        message=("You've lost! You're out the game. 😢")
+        userIsAlive = false
     }
 
+    console.log(message)
+    
