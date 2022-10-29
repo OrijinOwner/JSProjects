9d0dcc9363efb98f0b31ecfee160bdd8c7fc2d33
Ivan Gyimah <ivangyimah@ivans-mbp.mynet>
2022-10-29 17:20:49 +0100

    updates

---
+++ JS-concepts/typeconversion.js
@@ -0,0 +1,15 @@
+//Tell JS to change a data type from one to another 
+
+
+let a = 9;
+let b = "9";
+let c = a + b;
+console.log('Answer'  +c)
+
+
+
+// In the above example, JS trated both as string values or text values and is literally combining 9+9 to create 99. 
+
+//JS realises it cannot add a number and string together. STRING always wins!
+
+
